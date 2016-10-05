2442ed80917641f5087ed5b7683c3e029e9257d9
Alex Antonyuk <alex@antonyuk.me>
2016-10-05 09:55:45 +0200

    initial commit

---
+++ build_simulator.rb
@@ -0,0 +1,12 @@
+## Dummy build simulator
+
+puts "Starting building process of 🐿..."
+intervals_range = (10..20).to_a
+chosen_interval = intervals_range.sample
+puts "Assembling 🐿 for #{chosen_interval} seconds..."
+sleep chosen_interval
+error_possibility = Random.new.rand(2.0)
+if error_possibility > 1.5
+   fail "Error! Got some 💩!" 
+end
+puts "Success 🐿!"
