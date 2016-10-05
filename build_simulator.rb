fc69bf5db174fb1478b7f0e61ceb6b2219cc3d19
Alex Antonyuk <alex@antonyuk.me>
2016-10-05 09:58:55 +0200

    added output flushing

--- build_simulator.rb
+++ build_simulator.rb
@@ -4,9 +4,12 @@ puts "Starting building process of 🐿..."
 intervals_range = (10..20).to_a
 chosen_interval = intervals_range.sample
 puts "Assembling 🐿 for #{chosen_interval} seconds..."
+$stdout.flush
 sleep chosen_interval
+
 error_possibility = Random.new.rand(2.0)
 if error_possibility > 1.5
    fail "Error! Got some 💩!" 
 end
+
 puts "Success 🐿!"
