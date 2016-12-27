2e46543e561d3dfd91b30ff83734051245ce678f
Alex Antonyuk <alex@antonyuk.me>
2016-12-27 16:10:22 +0200

    no emoji

--- build_simulator.rb
+++ build_simulator.rb
@@ -1,19 +1,19 @@
 ## Dummy build simulator
 
-puts "Starting building process of 🐿..."
+puts "Starting building process of raccoon..."
 intervals_range = (10..20).to_a
 chosen_interval = intervals_range.sample
 
 is_fat = (ENV['IS_FAT'] || 0) ? "Fat" : "Skinny"
 color = ENV['COLOR'] || 'Brown'
 
-puts "Assembling [#{is_fat}, #{color}] 🐿 for #{chosen_interval} seconds..."
+puts "Assembling [#{is_fat}, #{color}] raccoon for #{chosen_interval} seconds..."
 $stdout.flush
 sleep chosen_interval
 
 error_possibility = Random.new.rand(2.0)
 if error_possibility > 1.5
-   fail "Error! Got some 💩!" 
+   fail "Error! Got some shiz!" 
 end
 
-puts "Success 🐿!"
+puts "Success raccoon!"
